1d2c0035c585325e5890be7ae5bd870f50b0a8f1
Sofia Campos <chofi_14_jscc@hotmail.com>
2018-12-14 05:54:34 -0500

    configurando event gameplay

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, function() { console.log('webhook is listening '+app.get('port')) } );
+app.listen(process.env.PORT || 5000, function(){ 
+  console.log('webhook is listening '+app.get('port')) 
+});
 
 // Server index page
 app.get("/", function (req, res) {
@@ -47,32 +49,6 @@ app.get("/", function (req, res) {
 
 });
 
-// Creates the endpoint for our webhook 
-app.post('/webhook/', (req, res) => {  
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
 // Adds support for GET requests to our webhook
 app.get('/webhook/', (req, res) => {
 
@@ -103,3 +79,132 @@ app.get('/webhook/', (req, res) => {
     res.send("No entry! Pruebita :)");
   }
 });
+
+// Creates the endpoint for our webhook 
+app.post('/webhook/', (req, res) => {  
+ 
+  let body = req.body;
+  console.log('received bot webhook');
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+          if (event.message) {
+              receivedMessage(event);
+          } else if (event.game_play) {
+              receivedGameplay(event);
+          } else {
+              console.log("Webhook received unknown event: ", event);
+          }
+      });
+      //let webhook_event = entry.messaging[0];
+      //console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+//
+// Handle messages sent by player directly to the game bot here
+//
+function receivedMessage(event) {
+  
+}
+
+//
+// Handle game_play (when player closes game) events here. 
+//
+function receivedGameplay(event) {
+    // Page-scoped ID of the bot user
+    var senderId = event.sender.id; 
+
+    // FBInstant player ID
+    var playerId = event.game_play.player_id; 
+
+    // FBInstant context ID 
+    var contextId = event.game_play.context_id;
+
+    // Check for payload
+    if (event.game_play.payload) {
+        //
+        // The variable payload here contains data set by
+        // FBInstant.setSessionData()
+        //
+        var payload = JSON.parse(event.game_play.payload);
+
+        // In this example, the bot is just "echoing" the message received
+        // immediately. In your game, you'll want to delay the bot messages
+        // to remind the user to play 1, 3, 7 days after game play, for example.
+        sendMessage(senderId, null, "Quieres juagr de nuevo?", "Jugar!", payload);
+    }
+}
+
+//
+// Send bot message
+//
+// player (string) : Page-scoped ID of the message recipient
+// context (string): FBInstant context ID. Opens the bot message in a specific context
+// message (string): Message text
+// cta (string): Button text
+// payload (object): Custom data that will be sent to game session
+// 
+function sendMessage(player, context, message, cta, payload) {
+    var button = {
+        type: "game_play",
+        title: cta
+    };
+
+    if (context) {
+        button.context = context;
+    }
+    if (payload) {
+        button.payload = JSON.stringify(payload)
+    }
+    var messageData = {
+        recipient: {
+            id: player
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [
+                    {
+                        title: message,
+                        buttons: [button]
+                    }
+                    ]
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+
+}
+
+function callSendAPI(messageData) {
+    var graphApiUrl = 'https://graph.facebook.com/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN;
+    request({
+        url: graphApiUrl,
+        method: "POST",
+        json: true,  
+        body: messageData
+    }, function (error, response, body){
+        console.error('send api returned', 'error', error, 'status code', response.statusCode, 'body', body);
+    });
+}
