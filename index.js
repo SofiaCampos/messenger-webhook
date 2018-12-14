d6c9d989ce9bf76b064cdd69d8024b2bfc119f94
Sofia Campos <chofi_14_jscc@hotmail.com>
2018-12-14 16:06:09 -0500

    configurando event game_play 20vo intento

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get("/", function (req, res) {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/bot', (req, res) => {
+app.get('/bote', (req, res) => {
 
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
@@ -82,7 +82,7 @@ app.get('/bot', (req, res) => {
 });
 
 // Creates the endpoint for our webhook 
-app.post('/bot', (req, res) => {  
+app.post('/bote', (req, res) => {  
  
   let body = req.body;
   console.log('received bot webhook');
