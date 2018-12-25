5be2b70339bfbb66c9e94724a88dd8c4ad995329
Sofia Campos <chofi_14_jscc@hotmail.com>
2018-12-25 17:55:14 -0500

    prime

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", function (req, res) {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook/', (req, res) => {
+app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
@@ -49,7 +49,7 @@ app.get('/webhook/', (req, res) => {
 });
 
 // Creates the endpoint for our webhook 
-app.post('/webhook/', (req, res) => {  
+app.post('/webhook', (req, res) => {  
  
   let body = req.body;
   console.log('received bot webhook');
